f4a08118323e373c94d65a21e19c7f0ebb622b10
Chinecherem <onyia.blessing2014@gmail.com>
2024-05-02 23:57:17 +0100

    Initial commit Shell scripting

---
+++ capstone_project.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Multiplication table
+
+#Prompt user to enter a number for the multiplication table
+read -p "Enter a number for the multiplication table: " num
+
+#Prompt user to enter table type
+echo "Do you want a full table or a partial table?"
+
+read -p "Enter 'f' for full and 'p' for partial: " choice
+
+if [[ "$choice" == "p" ]]; then
+	
+	echo "Enter the starting number (between 1 and 10) : "
+		read start
+
+	echo "Enter the ending number (between 1 and 10): "
+	read end
+
+	#Validate range input
+	if ! [[ "$start" =~ ^[0-9]+$ ]] || ! [[ "$end" =~ ^[0-9]+$ ]] || [[ "$start" -gt "$end" ]] || [[ "$start" -lt 1 ]] || [[ "$end" -gt 10 ]]; then
+		echo "Invalid range. Showing full table instead"
+
+	fi
+	# Display full table instead
+		start=1
+		end=10
+ elif [[ "$choice" == "f" ]]; then
+	 start=1
+	 end=10
+fi
+
+# 4. Generate and display multiplication table according to the specified range
+# Using C-stlye form 
+echo "Multiplication table for $num: C-style Form"
+echo "---------------------"
+for ((i=$start; i<=$end; i++)); do
+    echo "$num x $i = $((num*i))"
+done
+echo "---------------------"
+
+# Using List form
+echo "Multiplication table for $num: List Form"
+echo "---------------------"
+for i in $(seq $start $end); do
+    echo "$num x $i = $((num*i))"
+done
+echo "---------------------"
+
+
+
+
+
+	
+
